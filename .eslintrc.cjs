4d8900911adc558cacf483dd31e4848f95dbb533
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-07-09 12:51:03 +0200

    Configure Prettier and ESLint at monorepo root

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,28 +3,28 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
+		'prettier',
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	overrides: [
 		{
 			files: ['*.svelte'],
+			extends: ['plugin:svelte/recommended'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.svelte'],
+			},
+		},
+	],
 };
